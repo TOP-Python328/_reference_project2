6b1ed3072f4fa4ebed0c019b492f9b60a9a318df
Gennadiy <renardes@gmail.com>
2023-12-31 16:32:25 +0500

    ручной тест структур данных

--- test/manual/model.py
+++ test/manual/model.py
@@ -17,13 +17,21 @@ class DictOfRanges(dict):
                 raise ValueError('...')
         super().__init__(mappable)
     
-    def __getitem__(self, key):
+    def __getitem__(self, key: int):
         if isinstance(key, int):
             for left, right in self:
                 if left <= key <= right:
                     return super().__getitem__((left, right))
         else:
             return super().__getitem__(key)
+    
+    def get_range(self, key: int) -> tuple[int, int]:
+        if isinstance(key, int):
+            for left, right in self:
+                if left <= key <= right:
+                    return left, right
+        else:
+            raise TypeError
 
 
 @dataclass
@@ -47,7 +55,7 @@ class CreatureParameter(ABC):
         self.__value = initial
         self._min = left
         self._max = right
-        self.origin = creature
+        self.creature = creature
     
     @property
     def value(self) -> float:
@@ -66,17 +74,30 @@ class CreatureParameter(ABC):
         else:
             self.__value = new_value
     
-    # @abstractmethod
-    # def update(self) -> None:
-        # pass
+    @abstractmethod
+    def update(self) -> None:
+        pass
 
 
 class Health(CreatureParameter):
     name = 'здоровье'
+    
+    def update(self) -> None:
+        satiety = self.creature.params[Satiety]
+        critical = sum(satiety.range) / 4
+        if 0 < satiety.value < critical:
+            self.value -= 0.5
+        elif satiety.value == 0:
+            self.value -= 1
+        else:
+            self.value += 0.1
 
 
 class Satiety(CreatureParameter):
     name = 'сытость'
+    
+    def update(self) -> None:
+        self.value -= 1
 
 
 Parameters = Enum(
@@ -115,6 +136,22 @@ class Kind(DictOfRanges):
         super().__init__(phases)
 
 
+@dataclass
+class State:
+    age: int
+    
+    def __repr__(self):
+        return '/'.join(v for v in self.__dict__.values())
+
+
+class History(list):
+    def get_param(self, param: Type) -> list[float]:
+        return [
+            getattr(state, param.__name__)
+            for state in self
+        ]
+
+
 class Creature:
     def __init__(
             self, 
@@ -123,19 +160,60 @@ class Creature:
     ):
         self.kind = kind
         self.name = name
-        self.age: int = 0
+        self.__age: int = 0
         self.params: dict[Type, CreatureParameter] = {}
         for param in kind[0].params:
             cls = Parameters[param.name].value
             self.params[cls] = cls(
-                param.initial,
-                param.min,
-                param.max,
-                self
+                initial=param.initial,
+                left=param.min,
+                right=param.max,
+                creature=self,
             )
+        self.history: History = History()
     
     def __repr__(self):
-        return f'({self.kind.name}) {self.name}: {self.age} ИД'
+        title = f'({self.kind.name}) {self.name}: {self.age} ИД'
+        params = '\n'.join(
+            f'{p.name}: {p.value:.1f}' 
+            for p in self.params.values()
+        )
+        return f'{title}\n{params}'
+    
+    def update(self) -> None:
+        for param in self.params.values():
+            param.update()
+        self.save()
+    
+    @property
+    def age(self) -> int:
+        return self.__age
+    
+    @age.setter
+    def age(self, new_value: int):
+        old_phase = self.kind.get_range(self.__age)
+        new_phase = self.kind.get_range(new_value)
+        self.__age = new_value
+        if old_phase != new_phase:
+            self._grow_up()
+    
+    def _grow_up(self) -> None:
+        for param in self.kind[self.age].params:
+            cls = Parameters[param.name].value
+            initial = param.initial or self.params[cls].value
+            self.params[cls] = cls(
+                initial=initial,
+                left=param.min,
+                right=param.max,
+                creature=self,
+            )
+    
+    def save(self) -> State:
+        state = State(self.age)
+        for cls, param in self.params.items():
+            setattr(state, cls.__name__, param.value)
+        self.history.append(state)
+        return state
 
 
 dog = Kind(
